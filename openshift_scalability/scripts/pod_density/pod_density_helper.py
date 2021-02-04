0ac44a2a986132409b1e9fbf6ec4a7b2d2a4c73e
prubenda <prubenda@redhat.com>
2021-02-03 19:43:23 -0500

    starting docs

--- openshift_scalability/scripts/pod_density/pod_density_helper.py
+++ openshift_scalability/scripts/pod_density/pod_density_helper.py
@@ -59,8 +59,20 @@ def edit_machine_sets(worker_node_num):
                 break
 
 
+def pods_in_nodes():
+    nodes =run("oc get pods -A -o wide | grep svt | awk '{print $8}'")
+    node_list = nodes.split('\n')
+    node_json= {}
+    for node in node_list:
+        if node in node_json.keys():
+            continue
+        node_json[node] = node_list.count(node)
+
+    print(node_json)
+
 def get_pods_per_node():
 
+    #worker_nodes=$(oc get pods -A -o wide | grep svt | awk '{print $8}' | xargs)
     worker_node = run('oc get nodes | grep worker').strip()
     worker_node_list = worker_node.split("\n")
     for node in worker_node_list:
@@ -68,6 +80,56 @@ def get_pods_per_node():
         pods_in_node = run('oc get pods --all-namespaces -o wide | grep ' + str(node_name) + ' | grep svt -c').strip()
         print("There are " + str(pods_in_node) + " running in node " + str(node_name))
 
+def see_if_error_builds(output_file):
+    print('here')
+    errorbuilds=run('oc get builds --all-namespaces | grep svt | egrep -v "Running|Complete|Creating|Pending"')
+    with open(output_file, "a") as f:
+        f.write(str(errorbuilds))
+    print ("error builds" + str(errorbuilds))
+    COUNTER=0
+    error_builds_list = errorbuilds.split("\n")
+    builds = []
+
+    for val in error_builds_list:
+        COUNTER = 0
+        error_builds = []
+        line = re.split(r'\s{2,}', val)
+        for word in line:
+            if ((COUNTER % 6 ) == 0 ):
+                error_builds.append(word)
+            elif ((COUNTER % 6 ) == 1):
+
+                error_builds.append(word)
+                builds.append(error_builds)
+            else:
+                break
+
+            COUNTER += 1
+
+    return builds
+
+
+
+def get_error_builds(build_item):
+    namespace = build_item[0]
+    name = build_item[1]
+    #append to file
+    with open(name + namespace +".out", "w+") as f:
+        f.write("Describing info for " + name + " in namespace "+ namespace + '\n')
+        #logs = run("oc describe pod/" + str(name) + " -n " + namespace)
+        #f.write("Describe pod " + str(logs) + '\n')
+        logs = run("oc describe build " + str(name) + " -n " + namespace)
+        f.write("Describe build " + str(logs) + '\n')
+
+
+def check_error_build(global_output_file):
+    builds_list = see_if_error_builds(global_output_file)
+    skip_first = True
+    for build_item in builds_list:
+        if skip_first:
+            skip_first = False
+        else:
+            get_error_builds(build_item)
 
 def see_if_error(output_file):
     print('here')
@@ -102,7 +164,7 @@ def get_error_logs(pod_item, output_file):
     namespace = pod_item[0]
     name = pod_item[1]
     #append to file
-    with open(output_file, "a") as f:
+    with open(name + namespace +".out", "w+") as f:
         f.write("Debugging info for " + name + " in namespace "+ namespace + '\n')
         #logs = run("oc describe pod/" + str(name) + " -n " + namespace)
         #f.write("Describe pod " + str(logs) + '\n')
